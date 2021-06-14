851d66ca91ad7baed0be6dd4a746e2946b745b78
Listerine87 <brettjmill@gmail.com>
2021-06-13 17:59:47 -0700

    Initial commit Dojo Survey with Model assignment

---
+++ Models/Survey.cs
@@ -0,0 +1,6 @@
+public class Survey{
+    public string Name { get; set; }
+    public string Dojo { get; set; }
+    public string Language { get; set; }
+    public string Comment { get; set; }
+}
